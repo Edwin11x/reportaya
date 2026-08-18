75ddb57f33fad9a4bbd22e5298b75d0de59d8dc4
Edwin11x <45363652+Edwin11x@users.noreply.github.com>
2026-08-18 09:56:21 -0600

    Update report.dart

--- lib/features/reports/domain/entities/report.dart
+++ lib/features/reports/domain/entities/report.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class Report {
+  final String title;
+  final String description;
+  final String imageUrl;
+  final String status;
+  final String date;
+  final IconData icon;
+  final Color iconBackground;
+
+  const Report({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.status,
+    required this.date,
+    required this.icon,
+    required this.iconBackground,
+  });
+}
