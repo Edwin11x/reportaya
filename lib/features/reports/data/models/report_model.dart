0508a7acddd8a5e0172e1443eca7cd55bbf04ba0
Edwin11x <45363652+Edwin11x@users.noreply.github.com>
2026-08-18 09:55:19 -0600

    Update report_model.dart

--- lib/features/reports/data/models/report_model.dart
+++ lib/features/reports/data/models/report_model.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import '../../domain/entities/report.dart';
 
+class ReportModel extends Report {
+  const ReportModel({
+    required super.title,
+    required super.description,
+    required super.imageUrl,
+    required super.status,
+    required super.date,
+    required super.icon,
+    required super.iconBackground,
+  });
+}
