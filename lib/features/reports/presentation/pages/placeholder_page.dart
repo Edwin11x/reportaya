53b692b8a446a29713e0b139efe702b19be2745c
Edwin11x <45363652+Edwin11x@users.noreply.github.com>
2026-08-18 09:58:49 -0600

    Update placeholder_page.dart

--- lib/features/reports/presentation/pages/placeholder_page.dart
+++ lib/features/reports/presentation/pages/placeholder_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class PlaceholderPage extends StatelessWidget {
+  final String title;
+  const PlaceholderPage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(title, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w800)),
+    );
+  }
+}
